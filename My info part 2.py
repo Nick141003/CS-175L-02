0daa756cec1f96eb7f3e76656929254a6122b821
Nick141003 <114097034+Nick141003@users.noreply.github.com>
2023-01-19 08:43:44 -0500

    Add files via upload

---
+++ My info part 2.py
@@ -0,0 +1,6 @@
+# CS175L-02
+# Nicholas Stevens
+# My info
+print ("11 Brentwood Ave, Freehold New Jersey")
+print ("908-692-2822")
+print ("History")
---
+++ My info.py
@@ -0,0 +1,5 @@
+# CS175L-02
+# Nicholas Stevens
+# My info
+print ("11 Brentwood Ave, Freehold New Jersey")
+print ("908-692-2822")
