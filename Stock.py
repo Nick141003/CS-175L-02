8a58d3f292e98c61248706c2a9d6f5cdacee187b
Nick141003 <114097034+Nick141003@users.noreply.github.com>
2023-01-26 19:25:14 -0500

    Add files via upload

---
+++ Stock.py
@@ -0,0 +1,20 @@
+#Nicholas Stevens
+#CS175-02
+#stocks
+
+commision_rate = float(input("please enter commision rate"))
+NUM_SHARES = float(input("please enter num shares"))
+PURCHASE_PRICE = float(input("please enter purchase price"))
+SELLING_PRICE = float(input("please enter selling price"))
+amoutPaidForStock = NUM_SHARES * PURCHASE_PRICE
+purchaseComission = COMMSION_RATE * amoutPaidForStock
+totalPaid= amoutPaidForStock + purchaseComission
+stockSoldFor= NUM SHARES + Selling Price
+sellingCommission = COMMISSION_RATE + stockSoldFor
+totalReceived= stockSoldFor - selling commission
+profitOrLoss = totalRecieved - sellingComission
+print(input('Amout paid for stock: $'))
+print(input('Comission piad for purchase: $'))
+print(input('Amout the stock sold for: $'))
+print(input('Comission paid for on the sale:'))
+print(input('Profit for loss if negative:'))
