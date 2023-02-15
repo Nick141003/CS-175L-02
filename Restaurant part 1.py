f21b65d45366705cd47653f973cac1ccbbe97061
Nick141003 <114097034+Nick141003@users.noreply.github.com>
2023-02-14 19:34:06 -0500

    Add files via upload

--- Restaurant part 1.py
+++ Restaurant part 1.py
@@ -1,21 +1,21 @@
 #Nicholas Stevens
 #CS 175L 02
-#restaurant Part 1
+#restaurant Part 2
 
-input('Is anyone in your party a vegetarian?')
-input('Is anyone in your party vegan?')
-input('Is anyone in your party gluton free?')
+vegetarian = input('Is anyone in your party a vegetarian?')
+vegan = input('Is anyone in your party vegan?')
+glutonFree = input('Is anyone in your party glutonFree?')
 
-if vegetarian == 'yes' and vegan == 'yes' and gluten_free == 'yes':
+while vegetarian == "yes" and vegan == "yes" and glutonFree == "yes":
     print("Here are your restaurant choices:"
           "Corner Cafe"
           "The Chef's Kitchen")
-if vegetarian == 'no' and vegan == 'no' and gluten_free == 'no':
+if vegetarian == "no" and vegan == "no" and glutonFree == "no":
     print("Here are your restaurant choices:"
           "Joe's Gourmet Burgers")
-if vegetarian == 'yes' and vegan == 'no' and gluten_free == 'no':
+if vegetarian == "yes" and vegan == "no" and glutonFree == "no":
     print("Here are your restaurant choices:"
           "Mamas Fine Italian")
-if vegetarian == 'yes' and vegan == 'no' and gluten_free == 'yes':
+if vegetarian == "yes" and vegan == "no" and glutonFree == "yes":
     print("Here are your restaurant choices:"
           "Main Street Pizza Company")
