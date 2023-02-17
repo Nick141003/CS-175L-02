ac5154d7713c64e2838553da26ab66e8331116b4
Nick141003 <114097034+Nick141003@users.noreply.github.com>
2023-02-17 07:42:07 -0500

    Add files via upload

---
+++ stop.py
@@ -0,0 +1,26 @@
+#Nicholas Stevens
+#CS 175L 02
+#Stop
+import turtle  
+
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 400
+ANIMATION_SPEED = 0
+NUM_SIDES = 8
+LENGTH = 100
+ANGLE = 45
+TEXT_X = -5
+TEXT_Y = -10
+
+turtle.setup(WINDOW_WIDTH, WINDOW_HEIGHT)
+()
+turtle.color('red')
+turtle.sety(100)
+for x in range(8):
+    turtle.forward(100)
+    turtle.right(45)
+turtle.fillcolor("red")
+turtle.begin_fill()
+turtle.sety(-25)
+turtle.setx(50)
+turtle.write("stop")
